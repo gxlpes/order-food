8ae49a7d253c305ce689548c33bd09d530c7a050
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-08-09 09:03:57 -0300

    Adding useContext and useReducer

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,32 @@
 import { FormContainer } from "./Styles/MealItemFormStyles";
 import Input from "../../UI/Input";
+import { useRef, useState } from "react";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef;
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountConvertNumber = +enteredAmount; // convert string to a number
+
+    if (
+      enteredAmountConvertNumber.trim().length === 0 ||
+      enteredAmountConvertNumber < 1 ||
+      enteredAmountConvertNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountConvertNumber);
+  };
+
   return (
-    <FormContainer>
+    <FormContainer onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
@@ -16,6 +38,7 @@ const MealItemForm = (props) => {
         }} // usage of min and max to make a validation of the input amount
       />
       <button>Add Meal</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </FormContainer>
   );
 };
